69b2cf3dbe25a11631e35e87b1466f9c22d41583
Hyprlynk <kaesholk@gmail.com>
2021-10-19 09:03:58 -0400

    added proper architecture for actions

--- src/engine.py
+++ src/engine.py
@@ -3,7 +3,6 @@ from typing import Set, Iterable, Any
 from tcod.context import Context
 from tcod.console import Console
 
-from actions import EscapeAction, MovementAction
 from entity import Entity
 from game_map import GameMap
 from input_handlers import EventHandler
@@ -19,22 +18,15 @@ class Engine:
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
             action = self.event_handler.dispatch(event)
-
             if action is None:
                 continue
-
-            if isinstance(action, MovementAction):
-                if self.game_map.tiles["walkable"][self.player.x + action.dx, self.player.y + action.dy]:
-                    self.player.move(dx=action.dx, dy=action.dy)
-
-            elif isinstance(action, EscapeAction):
-                raise SystemExit()
+            action.perform(self, self.player)
 
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
 
         for entity in self.entities:
             console.print(entity.x, entity.y, entity.char, fg=entity.color)
-            
+
         context.present(console)
         console.clear()
