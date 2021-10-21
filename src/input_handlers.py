74bf8b81400c751b021439fa0573943a5bca3153
Hyprlynk <kaesholk@gmail.com>
2021-10-21 09:23:32 -0400

    added components for ai and fighting, changed input handling, etc.

--- src/input_handlers.py
+++ src/input_handlers.py
@@ -3,10 +3,47 @@ from __future__ import annotations
 from typing import Optional, TYPE_CHECKING
 import tcod.event
 
-from actions import Action, EscapeAction, BumpAction
+from actions import Action, BumpAction, EscapeAction, WaitAction
 
 if TYPE_CHECKING:
     from engine import Engine
+
+
+MOVE_KEYS = {
+    # Arrow keys.
+    tcod.event.K_UP: (0, -1),
+    tcod.event.K_DOWN: (0, 1),
+    tcod.event.K_LEFT: (-1, 0),
+    tcod.event.K_RIGHT: (1, 0),
+    tcod.event.K_HOME: (-1, -1),
+    tcod.event.K_END: (-1, 1),
+    tcod.event.K_PAGEUP: (1, -1),
+    tcod.event.K_PAGEDOWN: (1, 1),
+    # Numpad keys.
+    tcod.event.K_KP_1: (-1, 1),
+    tcod.event.K_KP_2: (0, 1),
+    tcod.event.K_KP_3: (1, 1),
+    tcod.event.K_KP_4: (-1, 0),
+    tcod.event.K_KP_6: (1, 0),
+    tcod.event.K_KP_7: (-1, -1),
+    tcod.event.K_KP_8: (0, -1),
+    tcod.event.K_KP_9: (1, -1),
+    # Vi keys.
+    tcod.event.K_h: (-1, 0),
+    tcod.event.K_j: (0, 1),
+    tcod.event.K_k: (0, -1),
+    tcod.event.K_l: (1, 0),
+    tcod.event.K_y: (-1, -1),
+    tcod.event.K_u: (1, -1),
+    tcod.event.K_b: (-1, 1),
+    tcod.event.K_n: (1, 1),
+}
+
+WAIT_KEYS = {
+    tcod.event.K_PERIOD,
+    tcod.event.K_KP_5,
+    tcod.event.K_CLEAR,
+}
 
 
 class EventHandler(tcod.event.EventDispatch[Action]):
@@ -36,34 +73,11 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         player = self.engine.player
 
         # Numpad movement
-        if key == tcod.event.K_KP_1:
-            action = BumpAction(player, dx=-1, dy=1)
-        elif key == tcod.event.K_KP_2:
-            action = BumpAction(player, dx=0, dy=1)
-        elif key == tcod.event.K_KP_3:
-            action = BumpAction(player, dx=1, dy=1)
-        elif key == tcod.event.K_KP_4:
-            action = BumpAction(player, dx=-1, dy=0)
-        elif key == tcod.event.K_KP_5:
-            action = BumpAction(player, dx=0, dy=0)
-        elif key == tcod.event.K_KP_6:
-            action = BumpAction(player, dx=1, dy=0)
-        elif key == tcod.event.K_KP_7:
-            action = BumpAction(player, dx=-1, dy=-1)
-        elif key == tcod.event.K_KP_8:
-            action = BumpAction(player, dx=0, dy=-1)
-        elif key == tcod.event.K_KP_9:
-            action = BumpAction(player, dx=1, dy=-1)
-
-        # Arrow keys
-        elif key == tcod.event.K_UP:
-            action = BumpAction(player, dx=0, dy=-1)
-        elif key == tcod.event.K_DOWN:
-            action = BumpAction(player, dx=0, dy=1)
-        elif key == tcod.event.K_LEFT:
-            action = BumpAction(player, dx=-1, dy=0)
-        elif key == tcod.event.K_RIGHT:
-            action = BumpAction(player, dx=1, dy=0)
+        if key in MOVE_KEYS:
+            dx, dy = MOVE_KEYS[key]
+            action = BumpAction(player, dx, dy)
+        elif key in WAIT_KEYS:
+            action = WaitAction(player)
 
         elif key == tcod.event.K_ESCAPE:
             action = EscapeAction(player)
