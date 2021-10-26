fc461b6f2aa400b10225be228c52a655af6ba640
Hyprlynk <kaesholk@gmail.com>
2021-10-26 09:11:37 -0400

    added colors, health bar, message log, mouseover text for entities, message history

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import copy
 
 import tcod
 
+import color
 from engine import Engine
 from entity import Entity
 import entity_factories
@@ -14,7 +15,7 @@ def main() -> None:
     screen_height = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 2
@@ -41,6 +42,10 @@ def main() -> None:
     )
     engine.update_fov()
 
+    engine.message_log.add_message(
+        "Welcome to Toad Hill!", color.welcome_text
+    )
+
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
@@ -50,8 +55,10 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            engine.render(console=root_console, context=context)
-            engine.event_handler.handle_events()
+            root_console.clear()
+            engine.event_handler.on_render(console=root_console)
+            context.present(root_console)
+            engine.event_handler.handle_events(context)
 
 
 if __name__ == "__main__":
