4b9a899d3cbf40450fada1f41728dbf2d13f3485
Hyprlynk <kaesholk@gmail.com>
2021-10-29 08:49:11 -0400

    add items and the massive pile of code needed to implement that kind of thing

--- src/main.py
+++ src/main.py
@@ -23,6 +23,7 @@ def main() -> None:
     max_rooms = 36
 
     max_monsters_per_room = 2
+    max_items_per_room = 2
 
     tileset = tcod.tileset.load_tilesheet(
         "tiles.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -39,6 +40,7 @@ def main() -> None:
         map_width=map_width,
         map_height=map_height,
         max_monsters_per_room=max_monsters_per_room,
+        max_items_per_room=max_items_per_room,
         engine=engine,
     )
     engine.update_fov()
